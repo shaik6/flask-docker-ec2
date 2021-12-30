7150fb51357c9b2a7c8078cde2dd7aa6ccbd5e9e
shaik6 <89659250+shaik6@users.noreply.github.com>
2021-12-30 17:56:47 +0530

    Update app.py

--- flaskapp-docker/flaskapp/app.py
+++ flaskapp-docker/flaskapp/app.py
@@ -1,14 +1,9 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("home.html")
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-@app.route("/home")
-def home():
-    return redirect(url_for('login'))
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
+if __name__ == "__main__":
+   app.run(host='0.0.0.0') 
